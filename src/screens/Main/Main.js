213f0366e3e54c024565f56f3612e428f8a227f4
Lê Chẳng <lechangbl@gmail.com>
2021-10-10 17:35:31 +0700

    documents

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -14,70 +14,17 @@ import Shop from './Shop/Shop';
 const Drawer = createDrawerNavigator();
 
 const Main = ({ navigation }) => {
-   // const Main = () => {
    return (
-      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-         <Text style={{
-            fontSize: fontsize.h3,
-            color: colors.backgroundGreen
-         }}>
-            MAIN
-         </Text>
-
-         <TouchableOpacity
-            style={{
-               backgroundColor: colors.backgroundGreen,
-               height: 60,
-               width: 300,
-               borderRadius: 10,
-               margin: 20,
-               justifyContent: 'center',
-               alignItems: 'center'
-            }}
-            onPress={() => {
-               navigation.navigate('Authentication')
-            }}
-         >
-            <Text style={{
-               fontSize: 30,
-               color: 'white',
-               alignItems: 'center'
-            }}>
-               goto Authentication
-            </Text>
-         </TouchableOpacity>
-
-         <TouchableOpacity
-            style={{
-               backgroundColor: colors.backgroundGreen,
-               height: 60,
-               width: 300,
-               borderRadius: 10,
-               margin: 20,
-               justifyContent: 'center',
-               alignItems: 'center'
-            }}
-            onPress={() => {
-               navigation.navigate('OderHistory')
-            }}
-         >
-            <Text style={{
-               fontSize: 30,
-               color: 'white',
-               alignItems: 'center'
-            }}>
-               goto OderHistory
-            </Text>
-         </TouchableOpacity>
-
-      </View>
-      // <NavigationContainer>
-
-      //    <Drawer.Navigator>
-      //       <Drawer.Screen name="Feed" component={Menu} />
-      //       <Drawer.Screen name="Article" component={Shop} />
-      //    </Drawer.Navigator>
-      // </NavigationContainer>
+      <Drawer.Navigator>
+         <Drawer.Screen
+            name="Shop"
+            component={Shop}
+         />
+         <Drawer.Screen
+            name="Menu"
+            component={Menu}
+         />
+      </Drawer.Navigator>
    );
 };
 
